c7e30d2faddd4bac19144d668cd67b37c2ef141e
jo9182 <73309930+jo9182@users.noreply.github.com>
2025-04-17 12:40:25 +0000

    Forgot to turn in :')

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,27 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
-
+		Arrays.sort(ray);
+		int bottom = 0;
+		int mid = 0;
+		int top = ray.length - 1;
+		int secret = item;
+		while(bottom <= top)
+		{
+			mid = (bottom + top) / 2;
+			if(ray[mid] < secret)
+			{
+				bottom = mid + 1;
+			}
+			else if(ray[mid] > secret)
+			{
+				top = mid - 1;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+		return -1;
 	}
 }
